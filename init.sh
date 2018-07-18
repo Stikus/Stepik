edae02e0c485962a0ad37e35010db8e7d11248e4
Konstantin Grammatikati <stikus@mail.ru>
2018-07-18 19:04:13 +0300

    init change

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-sudo ﻿ln -sf ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/tdefault
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/tdefault
 sudo /etc/init.d/nginx restart
-sudo ln -sf ~/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
