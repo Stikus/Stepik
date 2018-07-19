b42c417c6da888e901903356f57e9f777ad75c8e
Konstantin Grammatikati <stikus@mail.ru>
2018-07-19 11:45:14 +0300

    guni all

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
 def app(environ, start_response):
         body = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(body)))
-        ])
+        start_response("200 OK", [("Content-Type", "text/plain")])
         return body
