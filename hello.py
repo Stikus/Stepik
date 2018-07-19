3c514d749c61a4722940032e8a2176294a1038a8
Konstantin Grammatikati <stikus@mail.ru>
2018-07-19 13:07:05 +0300

    guni all

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
-        body = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
+        body = [(i+'\n') for i in environ['QUERY_STRING'].split('&')]
         start_response("200 OK", [("Content-Type", "text/plain")])
         return body
