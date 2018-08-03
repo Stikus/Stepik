ae525aadb0c65ecf289c963f5ca17c8b9c5972ad
Konstantin Grammatikati <stikus@mail.ru>
2018-08-03 12:17:27 +0300

    django2

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from qa.views import test
 
-urlpatterns = [                                              
-   url(r'^$', test),                                                              
-   url(r'^login/.*$', test, name='login'),                                    
-   url(r'^signup/.*', test, name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),                 
-   url(r'^ask/.*', test, name='ask'),                                         
-   url(r'^popular/.*', test, name='popular'),                                 
-   url(r'^new/.*', test, name='new')                                         
+urlpatterns = [
+    path('', test, name='home'),
+    path('login/', test, name='login'),
+    path('signup/', test, name='signup'),
+    path('question/<int:id>/', test, name='question'),
+    path('ask/', test, name='ask'),
+    path('popular/', test, name='popular'),
+    path('new/', test, name='new'),
 ]
--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+touch /home/box/web/gunicorn.log
 sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test
 sudo service gunicorn restart
+
+#sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
