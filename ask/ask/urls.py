4fd747643c5811dbce31e299e515205edba11351
Konstantin Grammatikati <stikus@mail.ru>
2018-08-03 14:12:16 +0300

    test3

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from qa.views import test
 
+"""
 urlpatterns = [
     path('', test, name='home'),
     path('login/', test, name='login'),
@@ -25,4 +26,14 @@ urlpatterns = [
     path('ask/', test, name='ask'),
     path('popular/', test, name='popular'),
     path('new/', test, name='new'),
+]
+"""
+urlpatterns = [
+    url(r'^$', test),
+    url(r'^login/.*$', test, name='login'), 
+    url(r'^signup/.*$', test, name='signup'), 
+    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
+    url(r'^ask/.*$', test, name='ask'),
+    url(r'^popular/.*', test, name='popular'),
+    url(r'^new/.*', test, name='new'),
 ]
