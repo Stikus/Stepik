55758176632e0d7ac98b6919834f59abe98c08f0
Konstantin Grammatikati <stikus@mail.ru>
2018-08-03 14:50:51 +0300

    1

--- init.sh
+++ init.sh
@@ -3,9 +3,8 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 touch /home/box/web/gunicorn.log
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/guni
 sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test
 sudo service gunicorn restart
-#sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
-#sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
 
 
